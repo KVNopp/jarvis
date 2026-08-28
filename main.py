bfb60a32937a832fabe782555ad40b0e9d9077b9
keven oproprio <kevenhenrique730@gmail.com>
2026-08-28 19:32:08 -0300

    adicionado funções

--- main.py
+++ main.py
@@ -21,39 +21,39 @@ else:
             print("Senha incorreta! Tente novamente.")
             time.sleep(3+1)  # Pausa de 1 segundo antes de permitir nova tentativa
 print(f"Olá, {nome}! Bem-vindo(a) ao programa.")
-print('como eu posso ajuadar você?')
-time.sleep(2)
-print('1 - Para saber a hora atual')
-print('2 - Para saber a data atual')
-print('3 - Para saber a sua idade em dias')
+
+
+
+
+def menu():
+    print('como eu posso ajuadar você?')
+    time.sleep(2)
+    print('1 - Para saber a hora atual')
+    print('2 - Para saber a data atual')
+    print('3 - Para saber a sua idade em dias')
+menu()
+menu
+
 hora = time.localtime()
 data = time.localtime()
 idade_dias = idade * 365
-opcao = int(input("Digite a opção desejada: "))
-if opcao == 1:
-    print(f"Hora atual: {hora.tm_hour}:{hora.tm_min}:{hora.tm_sec}")
-elif opcao == 2:
-    print(f"Data atual: {data.tm_mday}/{data.tm_mon}/{data.tm_year}")
-elif opcao == 3:
-    print(f"Sua idade em dias é: {idade_dias} dias")
 
+def script():
+    opcao = int(input("Digite a opção desejada: "))
+    if opcao == 1:
+        print(f"Hora atual: {hora.tm_hour}:{hora.tm_min}:{hora.tm_sec}")
+    elif opcao == 2:
+        print(f"Data atual: {data.tm_mday}/{data.tm_mon}/{data.tm_year}")
+    elif opcao == 3:
+        print(f"Sua idade em dias é: {idade_dias} dias")
+script()
+script
 while True:
         resposta = input("Deseja continuar? (s/n): ")
         if resposta.lower() == 's':
-            print('como eu posso ajuadar você?')
-            time.sleep(2)
-            print('1 - Para saber a hora atual')
-            print('2 - Para saber a data atual')
-            print('3 - Para saber a sua idade em dias')
-            opcao = int(input("Digite a opção desejada: "))
-            if opcao == 1:
-                print(f"Hora atual: {hora.tm_hour}:{hora.tm_min}:{hora.tm_sec}")
-            elif opcao == 2:
-                print(f"Data atual: {data.tm_mday}/{data.tm_mon}/{data.tm_year}")
-            elif opcao == 3:
-                print(f"Sua idade em dias é: {idade_dias} dias")
+             print("Continuando o programa...")
+             menu()
+             script()
         elif resposta.lower() == 'n':
-            print("Encerrando o programa. Até logo!")
+            print("Encerrando o programa...")
             break
-        else:
-            print("Opção inválida. Digite 's' para sim ou 'n' para não.")
